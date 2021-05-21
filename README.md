1277873cf408e3251523009ea90fba97bf64a1fe
nickFalcone <nfalcone@u.rochester.edu>
2021-05-21 16:18:28 -0400

    messaging for no conflicts

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Git Conflict Bot
+
+Preamble
+
+## Install
+
+Clone the repository and create an `endpoint.txt` file to hold your Slack webhook URL:
+
+```bash
+git clone git@github.com:nickFalcone/git-conflict-bot.git
+cd git-conflict-bot/
+chmod +x conflicts.sh
+echo -e "<your Slack webhook URL>" >> endpoint.txt
+```
+
+## Run
+
+```bash
+./conflicts.sh
+```
--- conflicts.sh
+++ conflicts.sh
@@ -2,13 +2,13 @@
 
 # A script to notify a Slack channel about git conflicts
 
+# TODO: read these in prompt or as argument?
+repo_path=~/projects/conflicted-repo/
 dest_branch=master
 incoming_branch=conflicts
-
 slack_endpoint=$(<endpoint.txt) # paste Slack webhook URL into `endpoint.txt` to keep private
-echo $slack_endpoint
 
-cd ~/projects/conflicted-repo/
+cd $repo_path
 
 git reset --hard HEAD && git clean -fd
 git fetch && git checkout $dest_branch && git pull && git pull origin $incoming_branch
@@ -21,7 +21,13 @@ for file in "${files[@]}"; do
   file_table+="| $file |\n"
 done
 
+if [ ${#files[@]} -eq 0 ]; then
+  message="No conflicts from $dest_branch to $incoming_branch :tada:"
+else
+  message="The following file(s) from $dest_branch to $incoming_branch are conflicted:\n$file_table"
+fi
 
+# TODO: formatting on Slack looks like garbage
 payload=$(cat <<EOF
 {
   "text": "The following files from $dest_branch to $incoming_branch are conflicted:\n$file_table"
@@ -29,6 +35,8 @@ payload=$(cat <<EOF
 EOF
 )
 
+# POST to Slack webhook URL. Re-attempt up to 10 times if failed. 
 curl --connect-timeout 10 --retry 10 --retry-max-time 600 -i -X POST -H 'Content-type: application/json' -d "$payload" "$slack_endpoint"
 
+# Bring repo back to HEAD
 git reset --hard HEAD && git clean -fd
