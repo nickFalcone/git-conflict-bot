abdc8d922a49659847fbbce78d57649d66f16eee
nickFalcone <nfalcone@u.rochester.edu>
2021-05-21 23:09:04 -0400

    run shfmt -l -w conflicts.sh

--- conflicts.sh
+++ conflicts.sh
@@ -21,21 +21,22 @@ files=($(git diff --name-only --diff-filter=U))
 files_list=""
 
 for file in "${files[@]}"; do
-  files_list+="\`$file\`\n"
+	files_list+="\`$file\`\n"
 done
 
 if [[ ${#files[@]} -eq 0 ]]; then
-  message="No conflicts from the \`$incoming_branch\` branch to the \`$dest_branch\` branch :tada:"
+	message="No conflicts from the \`$incoming_branch\` branch to the \`$dest_branch\` branch :tada:"
 else
-  message="The following file(s) from the \`$incoming_branch\` branch to the \`$dest_branch\` branch are conflicted:\n$files_list"
+	message="The following file(s) from the \`$incoming_branch\` branch to the \`$dest_branch\` branch are conflicted:\n$files_list"
 fi
 
-payload=$(cat <<EOF
+payload=$(
+	cat <<EOF
 {
   "text": "$message"
 }
 EOF
 )
 
-# POST to Slack webhook URL. Re-attempt up to 10 times if failed. 
+# POST to Slack webhook URL. Re-attempt up to 10 times if failed.
 curl --connect-timeout 10 --retry 10 --retry-max-time 600 -i -X POST -H 'Content-type: application/json' -d "$payload" "$slack_endpoint"
