ce875796341d66d819f751ff9b70077b9bf080d9
nickFalcone <nfalcone@u.rochester.edu>
2021-06-08 01:12:55 -0400

    Accept incoming and destination branch arguments, exit if either is not supplied; exit if target repo working tree not clean; run git reset after attempting local merge

--- conflicts.sh
+++ conflicts.sh
@@ -2,20 +2,42 @@
 
 # A script to notify a Slack channel about git conflicts
 
-# TODO: read these in prompt or as argument?
+# TODO: modify repo_path to match the repository you are checking
 repo_path=~/projects/conflicted-repo/
-dest_branch=master
-incoming_branch=conflicts
 
-# Assign Slack webhook URL from git ignored file to keep it private
+while getopts ":d:i:" opt; do
+  case $opt in
+    d)
+      dest_branch=$OPTARG >&2
+      ;;
+		i)
+      incoming_branch=$OPTARG >&2
+      ;;
+  esac
+done
+
+# Exit if either branch argument is missing.
+if [ -z "$dest_branch" ] || [ -z "$incoming_branch" ]; then
+	printf 'Provide an incoming and destination branch. Eg:\n\n ./conflicts.sh -i development -d main\n\n' >&2
+	exit 1
+fi
+
+# Assign Slack webhook URL from git ignored file to keep it private.
 slack_endpoint=$(<endpoint.txt)
 
 cd $repo_path
 
-# Generate conflicts locally
+# Exit if target repository working tree is not clean.
+# We won't be able to perform the local merge.
+if [[ $(git diff --stat) != '' ]]; then
+  printf "Please resolve working tree in $repo_path before continuing.\n"
+	exit 1
+fi
+
+# Generate conflicts (if any) locally.
 git fetch && git checkout $dest_branch && git pull && git pull origin $incoming_branch
 
-# Build up list of conflicted files and create payload for Slack webhook
+# Build up list of conflicted files.
 files=($(git diff --name-only --diff-filter=U))
 
 files_list=""
@@ -24,6 +46,7 @@ for file in "${files[@]}"; do
 	files_list+="\`$file\`\n"
 done
 
+# Create payload for Slack webhook.
 if [[ ${#files[@]} -eq 0 ]]; then
 	message="No conflicts from the \`$incoming_branch\` branch to the \`$dest_branch\` branch :tada:"
 else
@@ -40,3 +63,9 @@ EOF
 
 # POST to Slack webhook URL. Re-attempt up to 10 times if failed.
 curl --connect-timeout 10 --retry 10 --retry-max-time 600 -i -X POST -H 'Content-type: application/json' -d "$payload" "$slack_endpoint"
+
+# Reset the target repository after the attempted local merge.
+# It should be a safe action since any prior saved but not
+# committed work in the repository would trigger the script to exit
+# before reaching this point. 
+git reset --hard HEAD && git clean -fd
